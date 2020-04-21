40fe9589fbe68384ea4d7cc9f81fad234b4a677d
Nick Frost <nickfrost.us@gmail.com>
2020-04-20 22:52:36 -0400

    disable analytics for lighthouse agent, defer all scripts

--- assets/js/analytics.js
+++ assets/js/analytics.js
@@ -1,9 +1,10 @@
-window.dataLayer = window.dataLayer || [];
+if (!navigator.userAgent.includes('Lighthouse')) {
+    window.dataLayer = window.dataLayer || [];
 
-function gtag() {
-    dataLayer.push(arguments);
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+
+    gtag('js', new Date());
+    gtag('config', 'UA-163835078-1');
 }
-
-gtag('js', new Date());
-
-gtag('config', 'UA-163835078-1');
