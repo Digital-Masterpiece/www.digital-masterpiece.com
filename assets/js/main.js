3962621240bbd5638030a40d5dd0264d00e1826d
Nick Frost <nickfrost.us@gmail.com>
2021-07-13 11:33:38 -0400

    cta refinements

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,9 @@
-console.log('Main JavaScript Loaded.')
+function sizeCTA() {
+    const cta = document.querySelector('.cta');
+    if (cta) {
+        cta.style.height = window.innerHeight.toString() + 'px';
+    }
+}
+
+sizeCTA();
+window.addEventListener('resize', sizeCTA)
